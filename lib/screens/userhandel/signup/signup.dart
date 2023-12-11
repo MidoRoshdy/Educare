9036f5d5e250de33b6c57a76ee1f6b1342e9c71d
Ahmed khalaf <153211405+ahmedmohamedkhalaf1@users.noreply.github.com>
2023-12-11 22:40:06 +0200

    .

--- lib/screens/userhandel/signup/signup.dart
+++ lib/screens/userhandel/signup/signup.dart
@@ -1,3 +1,4 @@
+import 'package:educare2/core/Assets.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -8,75 +9,85 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          Image.asset(
+            Assets.logox2,
+            width: 133.67,
+            height: 22,
+          ),
+        ],
         leading: IconButton(
             icon: const Icon(Iconsax.arrow_left_2),
             onPressed: () {
               Navigator.of(context).pop();
             }),
       ),
-      body: Container(
-        child: const Column(
-          children: [
-            Text(
-              "Register",
-              style: TextStyle(
-                  color: Color(0xff000000),
-                  fontSize: 29.05,
-                  fontWeight: FontWeight.w700),
+      body: Column(
+        children: [
+          Container(
+            child: const Column(
+              children: [
+                Text(
+                  "Register",
+                  style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 29.05,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "Please create an account to track your child",
+                  style: TextStyle(
+                      color: Color(0xff7D7D7D),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500),
+                ),
+              ],
             ),
-            Text(
-              "Please create an account to track your child",
-              style: TextStyle(
-                  color: Color(0xff7D7D7D),
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500),
-            ),
-          ],
-        ),
-        // padding: const EdgeInsets.symmetric(
-        //   horizontal: 20,
-        // ),
-        // width: MediaQuery.of(context).size.width,
-        //  child: Column(
-        //   children: [
-        //     TextFormField(
-        //       decoration: InputDecoration(
-        //         hintText: 'Educational code',
-        //         prefixIcon: Icon(
-        //           Iconsax.d_cube_scan,
-        //         ),
-        //         hintStyle: TextStyle(
-        //           fontSize: 14,
-        //           color: Color(0xffD9D9D9),
-        //         ),
-        //       ),
-        //     ),
-        //     TextFormField (
-        //       decoration: InputDecoration(
-        //         hintText: 'Email',
-        //         prefixIcon: Icon(
-        //           Iconsax.sms,
-        //         ),
-        //         hintStyle: TextStyle(
-        //           fontSize: 14,
-        //           color: Color(0xffD9D9D9),
-        //         ),
-        //       ),
-        //     ),
-        //     TextFormField(
-        //       decoration: InputDecoration(
-        //         hintText: 'Password',
-        //         prefixIcon: Icon(
-        //           Iconsax.eye_slash,
-        //         ),
-        //         hintStyle: TextStyle(
-        //           fontSize: 14,
-        //           color: Color(0xffD9D9D9),
-        //         ),
-        //       ),
-        //     ),
-        //   ],
-        // ),
+          ),
+          Divider(
+            height: 30,
+          ),
+          Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Educational code',
+                  prefixIcon: Icon(
+                    Iconsax.d_cube_scan,
+                  ),
+                  hintStyle: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xffD9D9D9),
+                  ),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: Icon(
+                    Iconsax.sms,
+                  ),
+                  hintStyle: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xffD9D9D9),
+                  ),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(
+                    Iconsax.lock,
+                  ),
+                  hintStyle: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xffD9D9D9),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
